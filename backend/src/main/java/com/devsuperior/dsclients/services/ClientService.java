4bcd338fcdcde4997199cf0027f37136ecb6e7a9
TiagoSouzaGarcia <tiago.s.garcia@gmail.com>
2021-09-23 19:56:34 -0300

    Configuration of test h2, created ClientDTO, ClientService, ClientRepository, ClientResource

---
+++ backend/src/main/java/com/devsuperior/dsclients/services/ClientService.java
@@ -0,0 +1,36 @@
+package com.devsuperior.dsclients.services;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.devsuperior.dsclients.dto.ClientDTO;
+import com.devsuperior.dsclients.entities.Client;
+import com.devsuperior.dsclients.repositories.ClientRepository;
+
+
+@Service
+public class ClientService {
+	
+	@Autowired
+	private ClientRepository repository;
+
+	@Transactional(readOnly = true)
+	public Page<ClientDTO> findAllPaged(PageRequest pageRequest) {
+		Page<Client> list = repository.findAll(pageRequest);
+		return list.map(x -> new ClientDTO(x));
+	}
+
+	public ClientDTO findById(Long id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public ClientDTO insert(ClientDTO dto) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
